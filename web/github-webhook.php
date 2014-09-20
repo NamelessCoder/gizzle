d8db317d971863723297cb13b821ba3cd17481d5
Claus Due <claus@namelesscoder.net>
2014-09-20 17:18:33 +0200

    [TASK] Ensure trimmed .secret file contents

--- web/github-webhook.php
+++ web/github-webhook.php
@@ -5,7 +5,7 @@ require_once '../vendor/autoload.php';
 $secretFile = '../.secret';
 $secret = NULL;
 if (TRUE === file_exists($secretFile)) {
-	$secret = file_get_contents($secretFile);
+	$secret = trim(file_get_contents($secretFile));
 }
 $data = file_get_contents('php://input');
 
