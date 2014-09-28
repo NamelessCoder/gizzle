aa897eb432eecaed0e045393058a64b75ee6c17f
Claus Due <claus@namelesscoder.net>
2014-09-28 03:06:23 +0200

    [TASK] Collect output instead of continuous echo

--- web/github-webhook.php
+++ web/github-webhook.php
@@ -4,6 +4,9 @@ require_once '../vendor/autoload.php';
 
 $secretFile = '../.secret';
 $secret = NULL;
+$output = array(
+	'messages' => array()
+);
 if (TRUE === file_exists($secretFile)) {
 	$secret = trim(file_get_contents($secretFile));
 }
@@ -16,21 +19,24 @@ if ('cli' === php_sapi_name()) {
 try {
 	$settingsFileArgument = $_GET['settings'];
 	if (TRUE === empty($settingsFileArgument)) {
-		processSettingsFile('Settings.yml', $data, $secret);
+		processSettingsFile('Settings.yml', $data, $secret, $output);
 	} elseif (FALSE === is_array($settingsFileArgument)) {
-		processSettingsFile($settingsFileArgument, $data, $secret);
+		processSettingsFile($settingsFileArgument, $data, $secret, $output);
 	} else {
 		foreach ($settingsFileArgument as $settingsFile) {
-			processSettingsFile($settingsFile, $data, $secret);
+			processSettingsFile($settingsFile, $data, $secret, $output);
 		}
 	}
+	header('Content-type: application/json');
 } catch (\RuntimeException $error) {
 	header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', TRUE, 500);
-	echo $error->getMessage() . ' (' . $error->getCode() . ')';
+	$output['messages'][] = $error->getMessage() . ' (' . $error->getCode() . ')';
 }
 
+echo json_encode($output, JSON_HEX_QUOT | JSON_PRETTY_PRINT);
 
-function processSettingsFile($settingsFile, $data, $secret) {
+
+function processSettingsFile($settingsFile, $data, $secret, &$output) {
 	$allowedPattern = '/[^a-z0-9\/]+\.yml/i';
 	$settingsFile = urldecode($settingsFile);
 	$settingsFile = trim($settingsFile, './\\'); // no absolutes or dot-files, including escaped ones.
@@ -38,14 +44,11 @@ function processSettingsFile($settingsFile, $data, $secret) {
 	$payload = new \NamelessCoder\Gizzle\Payload($data, $secret, $settingsFile);
 	$response = $payload->process();
 	if (0 === $response->getCode()) {
-		$output = $response->getOutput();
-		header('Content-type: application/json');
-		echo json_encode($output, JSON_HEX_QUOT | JSON_PRETTY_PRINT);
+		$output += $response->getOutput();
 	} else {
-		header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', TRUE, 500);
-		echo 'The following errors were reported:' . PHP_EOL;
+		$output['messages'][] = 'The following errors were reported:';
 		foreach ($response->getErrors() as $error) {
-			echo $error->getMessage() . ' (' . $error->getCode() . ')' . PHP_EOL;
+			$output['messages'][] = $error->getMessage() . ' (' . $error->getCode() . ')' . PHP_EOL;
 		}
 	}
 }
